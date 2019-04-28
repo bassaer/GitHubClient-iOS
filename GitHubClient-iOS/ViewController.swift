175ceb5f188769ec3ba4be8f296d8e59b2cd8cba
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-04-28 19:17:12 +0900

    Initial Commit

---
+++ GitHubClient-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubClient-iOS
+//
+//  Created by 中山翼 on 2019/04/28.
+//  Copyright © 2019 Tsubasa Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
